4f22d984451e17c696510f44701b1dd6bfc52310
AnotherNoName <65259492+AnotherNoName@users.noreply.github.com>
2022-08-27 14:17:31 -0400

    Fix 1.8.1

--- src/main/java/me/earth/earthhack/impl/modules/render/popchams/ListenerTotemPop.java
+++ src/main/java/me/earth/earthhack/impl/modules/render/popchams/ListenerTotemPop.java
@@ -1,13 +1,7 @@
 package me.earth.earthhack.impl.modules.render.popchams;
 
-import com.mojang.authlib.GameProfile;
 import me.earth.earthhack.impl.event.events.misc.TotemPopEvent;
 import me.earth.earthhack.impl.event.listeners.ModuleListener;
-import me.earth.earthhack.impl.util.minecraft.PlayerUtil;
-import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.entity.player.EntityPlayer;
-
-import java.util.UUID;
 
 final class ListenerTotemPop extends ModuleListener<PopChams, TotemPopEvent> {
     public ListenerTotemPop(PopChams module) {
@@ -19,13 +13,14 @@ final class ListenerTotemPop extends ModuleListener<PopChams, TotemPopEvent> {
     public void invoke(TotemPopEvent event) {
         if (!module.isValidEntity(event.getEntity()))
             return;
-        EntityPlayer player = event.getEntity();
 
-        module.getPopDataList().add(new PopChams.PopData(PlayerUtil.copyPlayer(event.getEntity(), module.copyAnimations.getValue()),
+        module.getPopDataHashMap().put(event.getEntity().getName(),
+                new PopChams.PopData(event.getEntity(),
                         System.currentTimeMillis(),
-                        player.posX,
-                        player.posY,
-                        player.posZ,
-                        player instanceof AbstractClientPlayer && ((AbstractClientPlayer)player).getSkinType().equals("slim") ));
+                        event.getEntity().rotationYaw,
+                        event.getEntity().rotationPitch,
+                        event.getEntity().posX,
+                        event.getEntity().posY,
+                        event.getEntity().posZ));
     }
 }
